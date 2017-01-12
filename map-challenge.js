10c71341e85228d61dd75197adbcb46907931316
MichaelMini <mcho911@gmail.com>
2017-01-12 22:16:03 +0000

    Removed comments

--- map-challenge.js
+++ map-challenge.js
@@ -17,22 +17,3 @@ var result = input.map(/* Your code here */
 console.log(result[0] === 5);
 console.log(result[1] === 13);
 console.log(result[2] === 17);
-
-// var kvArray = [{key:1, value:10},
-//                {key:2, value:20},
-//                {key:3, value: 30}];
-
-// var reformattedArray = kvArray.map(function(obj){
-//    var rObj = {};
-//    console.log(obj.key);
-//    console.log(obj.value);
-//    rObj[obj.key] = obj.value;
-// console.log(rObj);
-//    return rObj;
-// });
-// reformattedArray is now [{1:10}, {2:20}, {3:30}],
-
-// kvArray is still:
-// [{key:1, value:10},
-//  {key:2, value:20},
-//  {key:3, value: 30}]
